274944d1187380af10df8277d3f8607956dbdfbd
Sachin Gurung <saching061@hotmail.com>
2019-12-10 21:45:34 -0800

    First Commit

--- Compare Two Dates/Program.cs
+++ Compare Two Dates/Program.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Compare_Two_Dates
+﻿/*
+ * C# Program to Campare Two Dates
+ */
+using System;
+namespace DateAndTime
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main()
         {
+            DateTime sd = new DateTime(2010, 10, 12);
+            Console.WriteLine("Starting Date : {0}", sd);
+            DateTime ed = sd.AddDays(10);
+            Console.WriteLine("Ending Date   : {0}", ed);
+            if (sd < ed)
+                Console.WriteLine("{0} Occurs Before {1}", sd, ed);
+            Console.Read();
+            return 0;
+
         }
     }
 }
+/*
+Starting Date : 10/12/2010 12:00:00 AM
+Ending Date   : 10/22/2010 12:00:00 AM
+10/12/2010 12:00:00 AM Occurs Before 10/22/2010 12:00:00 AM
+
+Press any key to continue . . .
+*/
